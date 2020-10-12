ca3af24b7c6298118e1b73c3055a319edfcaa154
NicolasCalvet <nicolas.calvetgp@gmail.com>
2020-10-12 22:10:11 +0200

    LineRenderer devoir done

--- Anim_TP/Assets/Scripts/Lines.cs
+++ Anim_TP/Assets/Scripts/Lines.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Lines : MonoBehaviour
 {
 
+    public List<Vector3> vertices;
+    public List<int> lines;
+
     private LineRenderer lr;
 
     // Start is called before the first frame update
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        lr.positionCount = lines.Count;
+        for (int i = 0; i < lines.Count; i+=2) {
+            lr.SetPosition(i, vertices[lines[i]]);
+            lr.SetPosition(i+1, vertices[lines[i+1]]);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
