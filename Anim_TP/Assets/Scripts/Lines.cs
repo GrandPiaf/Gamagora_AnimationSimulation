7bb72331b9ce4547427b4b8c8e26643850dc0916
NicolasCalvet <nicolas.calvetgp@gmail.com>
2020-10-12 20:43:14 +0200

    Devoir lignes : recréation du projet en version plus récente

---
+++ Anim_TP/Assets/Scripts/Lines.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lines : MonoBehaviour
+{
+
+    private LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
