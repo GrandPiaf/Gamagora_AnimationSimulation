6541254a6417d3c9b22489550ad7e938ee5246d4
NicolasCalvet <nicolas.calvetgp@gmail.com>
2020-10-18 00:41:59 +0200

    Particle system : generating points/sprites

---
+++ Particle/Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+
+    public GameObject particle;
+
+    public int nbParticles;
+
+    private List<GameObject> particles;
+    
+    void Start() {
+
+        particles = new List<GameObject>(nbParticles);
+
+        for (int i = 0; i < nbParticles; i++) {
+            particles.Add( Instantiate(particle, new Vector3((UnityEngine.Random.value - 0.5f) * 16, (UnityEngine.Random.value - 0.5f) * 8), Quaternion.identity) );
+        }
+    }
+
+}
