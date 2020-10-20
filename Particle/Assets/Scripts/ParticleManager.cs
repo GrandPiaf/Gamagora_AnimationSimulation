da463e5a70580faea9f5db10ffbde38da7c11e39
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-10-20 11:08:28 +0200

    Particle : particle's color is set depending on its own mass

--- Particle/Assets/Scripts/ParticleManager.cs
+++ Particle/Assets/Scripts/ParticleManager.cs
@@ -10,21 +10,38 @@ public class ParticleManager : MonoBehaviour
     public int nbParticles;
 
     private List<GameObject> particles;
-    
+
+
+    private static float lowerMass = 1f;
+    private static float upperMass = 10f;
+
+    // setting color FROM mass
+    private static Color low = Color.blue;
+    private static Color high = Color.red;
+
+
+
     void Start() {
 
         particles = new List<GameObject>(nbParticles);
 
         for (int i = 0; i < nbParticles; i++) {
+
+            //Create particle i
             particles.Add( Instantiate(particle, new Vector3((Random.value - 0.5f) * 16, (Random.value - 0.5f) * 8), Quaternion.identity) );
-            particles[i].GetComponent<SpriteRenderer>().material.color = new Color(
-                Random.Range(0f, 1f),
-                Random.Range(0f, 1f),
-                Random.Range(0f, 1f),
-                1
-            );
-            particles[i].GetComponent<Particle>().mass = Random.Range(1f, 10f);
+            
+            // Settings velocity & random mass from range [1f, 10f]
+            float mass = Random.Range(lowerMass, upperMass);
+            particles[i].GetComponent<Particle>().mass = mass;
             particles[i].GetComponent<Particle>().velocity = (Vector3.up + Vector3.right) * 10f;
+
+            // Setting color depending on mass
+            // RED = high mass
+            // BLUE = low mass
+            float rangedMass = (((mass - lowerMass)) / (upperMass - lowerMass));
+            Color current = rangedMass * high + (1 - rangedMass) * low;
+
+            particles[i].GetComponent<SpriteRenderer>().material.color = current;
         }
     }
 
